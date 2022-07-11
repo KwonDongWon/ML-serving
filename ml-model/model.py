c1af01e6320e44330ab0ec1cfdb4033b8af8809d
KwonDongWon <sosdong@naver.com>
2022-07-11 14:23:20 +0900

    feat: add iris model

---
+++ ml-model/model.py
@@ -0,0 +1,27 @@
+"""
+iris data logistic regression modeling
+"""
+import pickle
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+def train_model():
+    iris = load_iris()  # sample data load
+    X = iris.data
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
+
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    model.score(X_test, y_test)
+    pickle.dump(model, open('model.pkl', 'wb'))
+
+
+def get_model():
+    model = pickle.load(open('model.pkl', 'rb'))
+    return model
+
